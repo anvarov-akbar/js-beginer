caf1da204bb3c98fd1fee5e06ba7839893fd3a7e
anvarov <anvarov9989@gmail.com>
2022-01-05 23:14:40 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+alert("Asalom aleykum sorovnomani toldiring!");
+
+var userName = prompt ("Ismingizni yozin?");
+
+var userSurname = prompt ("Familyangizni yozin?");
+
+var userFullName = userName+ " " +userSurname
+
+var age = prompt ("Yoshingizni yozin?" + userFullName);
+
+var workPalace = prompt ("Ish joyingiz yozin?");
+
+var fatherName = prompt ("Otangizni ismini yozin?");
+
+var motherName = prompt ("Onangizni ismini yozin?");
+
+var familyInfo = prompt ("aka ukangiz haqida yozin?");
+
+var education = prompt ("oqishingiz haqida yozin?");
+
+var adress = prompt ("adresingiz yozin?");
+
+console.log( "Ismingiz:" + userName + "\n" + "Familyangiz:" + userSurname + "\n" + "Yoshingiz:" + age + "\n" + "Is joyingiz:" + workPalace + "\n" + "Otangizning ismi:" + fatherName + "\n" + "Onangizni ismi:" + motherName + "\n" + "oylangiz haqida:" + familyInfo + "\n" + "oqish joyingiz:" + education + "\n" + "yashash joyingiz:" + adress ); 
